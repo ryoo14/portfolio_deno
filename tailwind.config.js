5089427a1c450aa5ee0efadb438f4dbe6a9bf485
ryoo14 <anana12185@gmail.com>
2024-01-07 12:17:16 +0900

    style: add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        ryorange: '#fd8850',
+      },
+    },
+  },
+  plugins: [],
+}
