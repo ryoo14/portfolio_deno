c1e2f52bc3e28ff61ae5106860e3a2f621654c33
ryoo14 <anana12185@gmail.com>
2024-01-10 19:02:31 +0900

    feat: display only the content of the selected link

---
+++ assets/static/main.js
@@ -0,0 +1,18 @@
+document.querySelectorAll('.sidebar-link').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    document.querySelectorAll('.ccc').forEach(c => {
+      c.style.display = "none";
+      c.classList.remove("visible");
+    });
+
+    const targetId = this.getAttribute("data-target");
+    const targetContent = document.getElementById(targetId);
+    targetContent.style.display = "grid";
+
+    setTimeout(() => {
+      targetContent.classList.add("visible");
+    }, 10);
+  });
+});
