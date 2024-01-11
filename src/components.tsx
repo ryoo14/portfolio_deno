b7d36577d205828b2d1247f81127d9f2cdf55a55
ryoo14 <anana12185@gmail.com>
2024-01-12 00:37:05 +0900

    style: handle responsive

--- src/components.tsx
+++ src/components.tsx
@@ -9,11 +9,12 @@ export const Home = (props) => {
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/tabler-icons.min.css" />
       </head>
       <body>
-        <div class="container text-rgray bg-rwhite flex justify-center lg:block mt-0 lg:mt-20">
+        <div class="container text-rgray bg-rwhite flex flex-col justify-center items-center w-full lg:flex-row lg:block mt-0 lg:mt-20">
           <SideBar />
-          <div class="main-content w-9/12 lg:absolute mt-40 lg:left-56 lg:mt-0">
+          <div class="main-content w-9/12 lg:absolute mt-24 mb-10 lg:left-56 lg:mt-0">
             { props.children }
           </div>
+          <Footer />
         </div>
         <script src="/static/main.js" />
       </body>
@@ -23,18 +24,12 @@ export const Home = (props) => {
 
 const SideBar = () => {
   return (
-    <div class="sidebar flex flex-row justify-between items-center lg:flex-col fixed lg:left-16 w-10/12 lg:w-auto z-10 bg-rwhite h-32">
+    <div class="sidebar flex flex-row justify-between items-center bg-rwhite/80 backdrop-blur-sm fixed top-0 lg:top-20 z-10 w-9/12 h-16 lg:flex-col lg:left-16 lg:w-24">
       <object data="/static/ryoop.svg" class="h-12 lg:mb-20" />
-      <div class="links flex flex-row w-3/6 justify-between lg:flex-col">
+      <div class="links flex flex-row w-3/6 lg:w-24 justify-between items-center lg:flex-col">
         <a href="#" class="sidebar-link hover:text-rorange lg:mb-10" data-target="work">Works</a>
         <a href="#" class="sidebar-link hover:text-rorange lg:mb-10" data-target="use">Uses</a>
         <a href="https://blog.ryoo.cc" target="_blank" rel="noopner noreferrer" class="sidebar-exlink hover:text-rorange lg:mb-10">Blog</a>
-      </div>
-      <div class="sns lg:mt-10 text-2xl">
-        <a href="https://twitter.com/ryoo141" target="_blanck" rel="noopner noreferrer"><i class="ti ti-brand-twitter hover:text-rblue" /></a>
-        <a href="https://github.com/ryoo14" target="_blanck" rel="noopner noreferrer"><i class="ti ti-brand-github hover:text-rblue" /></a>
-        <a href="https://www.instagram.com/ryoo141" target="_blanck" rel="noopner noreferrer"><i class="ti ti-brand-instagram hover:text-rblue" /></a>
-        <a href="https://atcoder.jp/users/ryoo14" target="_blanck" rel="noopner noreferrer"><i class="ti ti-code hover:text-rblue" /></a>
       </div>
     </div>
   )
@@ -67,4 +62,21 @@ type Content = {
   detail: string;
   thumbnail: string;
   url: string;
+}
+
+const Footer = () => {
+  const year = (new Date()).getFullYear()
+  return (
+    <div class="footer flex justify-between items-center w-9/12 bg-rwhite h-16 lg:flex-col lg:left-16 lg:w-24 lg:top-3/4 lg:fixed lg:z-10">
+      <div class="sns flex flex-row text-2xl">
+        <a href="https://twitter.com/ryoo141" target="_blanck" rel="noopner noreferrer"><i class="ti ti-brand-twitter hover:text-rblue" /></a>
+        <a href="https://github.com/ryoo14" target="_blanck" rel="noopner noreferrer"><i class="ti ti-brand-github hover:text-rblue" /></a>
+        <a href="https://www.instagram.com/ryoo141" target="_blanck" rel="noopner noreferrer"><i class="ti ti-brand-instagram hover:text-rblue" /></a>
+        <a href="https://atcoder.jp/users/ryoo14" target="_blanck" rel="noopner noreferrer"><i class="ti ti-code hover:text-rblue" /></a>
+      </div>
+      <div class="copyright flex flex-row">
+        © { year } ryoo 
+      </div>
+    </div>
+  )
 }
