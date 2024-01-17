c6173e9c0bd499be60eb3bc697e230a8cc7d5d37
ryoo14 <anana12185@gmail.com>
2024-01-17 14:22:54 +0900

    style: tweak

--- src/main.tsx
+++ src/main.tsx
@@ -16,7 +16,7 @@ const Home = (props) => {
       <body>
         <div class="container text-rgray bg-rwhite flex flex-col justify-start items-center w-full lg:flex-row lg:block mt-0 lg:mt-20">
           <SideBar />
-          <div class="main-content w-9/12 sm:w-11/12 lg:w-9/12 lg:absolute mt-24 sm:mt-36 mb-10 lg:left-56 lg:mt-0">
+          <div class="main-content w-9/12 sm:w-11/12 lg:w-9/12 lg:absolute mt-24 sm:mt-32 mb-10 lg:left-56 lg:mt-0">
             <About />
             { props.children }
           </div>
@@ -30,7 +30,7 @@ const Home = (props) => {
 
 const SideBar = () => {
   return (
-    <div class="sidebar flex flex-row justify-between items-center bg-rwhite/80 backdrop-blur-sm fixed top-0 lg:top-20 z-10 w-10/12 h-16 sm:w-11/12 sm:h-28 lg:flex-col lg:left-16 lg:w-24">
+    <div class="sidebar flex flex-row justify-between items-center bg-rwhite/80 backdrop-blur-sm fixed top-0 lg:top-20 z-10 w-10/12 h-16 sm:w-11/12 sm:h-20 lg:h-28 lg:flex-col lg:left-16 lg:w-24">
       <object data="/static/ryoop.svg" class="h-12 lg:mb-20" />
       <div class="links flex flex-row w-4/6 sm:w-3/6 lg:w-24 justify-between items-center lg:flex-col">
         <a href="#" class="sidebar-link hover:text-rorange lg:mb-10" data-target="about">About</a>
