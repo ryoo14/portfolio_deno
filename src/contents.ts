9749b38e1d5778c3f4f33364a5232c18bfdb6084
ryoo <anana12185@gmail.com>
2024-01-27 11:44:11 +0900

    add formatter (#5)
    * ci: Add formatter
    
    * style: Run fmt

--- src/contents.ts
+++ src/contents.ts
@@ -1,115 +1,115 @@
 export type Contents = {
-  type: string;
-  contents: Content[];
+  type: string
+  contents: Content[]
 }
 
 type Content = {
-  title: string;
-  overview: string;
-  detail: string;
-  thumbnail: string;
-  url: string;
+  title: string
+  overview: string
+  detail: string
+  thumbnail: string
+  url: string
 }
 
 export const work: Contents = {
   type: "work",
   contents: [
-    { 
+    {
       title: "HoloCut",
       overview: "Web App",
       detail: "Web App that collects clipped hololive videos.",
       thumbnail: "/static/holocutp.svg",
-      url: "https://holocut.fan"
+      url: "https://holocut.fan",
     },
-    { 
+    {
       title: "Patty",
       overview: "CLI Tool",
       detail: "A CLI Tool for managing git and working directories written in Deno.",
       thumbnail: "/static/pattyp.svg",
-      url: "https://github.com/ryoo14/patty"
+      url: "https://github.com/ryoo14/patty",
     },
-    { 
+    {
       title: "Nautitwilight.vim",
       overview: "Vim Colorscheme",
       detail: "Nautical Twilight color scheme for Vim.",
       thumbnail: "/static/nautilightp.svg",
-      url: "https://github.com/ryoo14/nautitwilight.vim"
+      url: "https://github.com/ryoo14/nautitwilight.vim",
     },
-    { 
+    {
       title: "Portfolio",
       overview: "Web App",
       detail: "My Portfolio",
       thumbnail: "/static/portfoliop.svg",
-      url: "https://ryoo.cc"
+      url: "https://ryoo.cc",
     },
-  ]
+  ],
 }
 
 export const use: Contents = {
   type: "use",
   contents: [
-    { 
+    {
       title: "Neo65",
       overview: "Keyboard",
       detail: "Primary Keyboard.",
       thumbnail: "https://d3toh8on7lf5va.cloudfront.net/neo65.jpg",
-      url: "https://qwertykeys.notion.site/Neo-65-6c8ae7895ec442dea809057c3dc5e113"
+      url: "https://qwertykeys.notion.site/Neo-65-6c8ae7895ec442dea809057c3dc5e113",
     },
-    { 
+    {
       title: "Nuphy Air60 v2",
       overview: "Keyboard",
       detail: "Keyboard for outside use.",
       thumbnail: "https://d3toh8on7lf5va.cloudfront.net/air60v2.jpg",
-      url: "https://nuphy.com/collections/keyboards/products/air60-v2"
+      url: "https://nuphy.com/collections/keyboards/products/air60-v2",
     },
-    { 
+    {
       title: "Planck EZ",
       overview: "Keyboard",
       detail: "40% cute keyboard for breather use.",
       thumbnail: "https://d3toh8on7lf5va.cloudfront.net/planckez.jpg",
-      url: "https://blog.zsa.io/2307-goodbye-planck-ez/"
+      url: "https://blog.zsa.io/2307-goodbye-planck-ez/",
     },
     {
-      title: "M2 MacBook Air 13\"",
+      title: 'M2 MacBook Air 13"',
       overview: "Laptop",
       detail: "Main machine.",
       thumbnail: "https://d3toh8on7lf5va.cloudfront.net/mba.jpg",
-      url: "https://www.apple.com/macbook-air-13-and-15-m2/"
+      url: "https://www.apple.com/macbook-air-13-and-15-m2/",
     },
     {
       title: "iPad Air4 & iPad mini6",
       overview: "Tablet",
       detail: "Photo Editing and Content Consumption.",
       thumbnail: "https://d3toh8on7lf5va.cloudfront.net/ipads.jpg",
-      url: "https://www.apple.com/ipad/"
+      url: "https://www.apple.com/ipad/",
     },
     {
       title: "Fujifilm X-E4",
       overview: "Camera",
       detail: "Snap a pic.",
       thumbnail: "https://d3toh8on7lf5va.cloudfront.net/xe4.jpg",
-      url: "https://fujifilm-x.com/ja-jp/products/cameras/x-e4/"
+      url: "https://fujifilm-x.com/ja-jp/products/cameras/x-e4/",
     },
     {
       title: "HOKA Clifton 9",
       overview: "Shoes",
       detail: "Buoyancy makes everyday running fun.",
       thumbnail: "https://d3toh8on7lf5va.cloudfront.net/clifton9.jpg",
-      url: "https://www.hoka.com/jp/clifton-9/1127895.html?dwvar_1127895_color=VRTL"
+      url: "https://www.hoka.com/jp/clifton-9/1127895.html?dwvar_1127895_color=VRTL",
     },
     {
       title: "Aer Sling Bag 3",
       overview: "Bag",
       detail: "Daily use",
       thumbnail: "https://d3toh8on7lf5va.cloudfront.net/aer.jpg",
-      url: "https://aersf.jp/products/detail/90"
+      url: "https://aersf.jp/products/detail/90",
     },
     {
       title: "Vim",
       overview: "Editor",
       detail: "Note Writing and Coding.",
       thumbnail: "https://d3toh8on7lf5va.cloudfront.net/vim.jpg",
-      url: "https://www.vim.org"
+      url: "https://www.vim.org",
     },
-  ]
+  ],
 }
--- src/main.tsx
+++ src/main.tsx
@@ -19,7 +19,7 @@ const Home = (props) => {
           <SideBar />
           <div class="main-content w-9/12 sm:w-11/12 lg:w-9/12 lg:absolute mt-24 sm:mt-32 mb-10 lg:left-56 lg:mt-0">
             <About />
-            { props.children }
+            {props.children}
           </div>
           <Footer />
         </div>
@@ -34,10 +34,18 @@ const SideBar = () => {
     <div class="sidebar flex flex-row justify-between items-center backdrop-blur-sm fixed top-0 lg:top-20 z-10 w-10/12 h-16 sm:w-11/12 sm:h-20 lg:h-28 lg:flex-col lg:left-16 lg:w-24">
       <object data="/static/portfoliop.svg" class="h-12 lg:mb-20" />
       <div class="links flex flex-row w-4/6 sm:w-3/6 lg:w-24 justify-between items-center lg:flex-col">
-        <a href="#" class="sidebar-link hover:text-rorange lg:mb-10" data-target="about">About</a>
-        <a href="#" class="sidebar-link hover:text-rorange lg:mb-10" data-target="work">Works</a>
-        <a href="#" class="sidebar-link hover:text-rorange lg:mb-10" data-target="use">Uses</a>
-        <a href="https://blog.ryoo.cc" target="_blank" rel="noopener noreferrer" class="sidebar-exlink hover:text-rorange lg:mb-10">Blog</a>
+        <a href="#" class="sidebar-link hover:text-rorange lg:mb-10" data-target="about">
+          About
+        </a>
+        <a href="#" class="sidebar-link hover:text-rorange lg:mb-10" data-target="work">
+          Works
+        </a>
+        <a href="#" class="sidebar-link hover:text-rorange lg:mb-10" data-target="use">
+          Uses
+        </a>
+        <a href="https://blog.ryoo.cc" target="_blank" rel="noopener noreferrer" class="sidebar-exlink hover:text-rorange lg:mb-10">
+          Blog
+        </a>
       </div>
     </div>
   )
@@ -50,20 +58,14 @@ const About = () => {
         <h2 class="text-2xl mr-2">Hi! I'm ryoo!</h2>
         <object data="/static/ryoop.svg" class="h-12" />
       </div>
-      <p class="text-lg mb-6">
-        I work as an Infrastructure Engineer in Japan and develop applications as a hobby.
-      </p>
-      <h2 class="text-2xl mb-2">
-        I ❤️
-      </h2>
+      <p class="text-lg mb-6">I work as an Infrastructure Engineer in Japan and develop applications as a hobby.</p>
+      <h2 class="text-2xl mb-2">I ❤️</h2>
       <ul class="text-lg mb-6 list-[square] list-inside">
         <li>Photography</li>
         <li>Coding Small Apps</li>
         <li>Tinkering with Mechanical Keyboards</li>
       </ul>
-      <h2 class="text-2xl mb-2">
-        My 📚
-      </h2>
+      <h2 class="text-2xl mb-2">My 📚</h2>
       <ul class="text-lg mb-6 list-[square] list-inside">
         <li>Linux Server: Setup and Operation</li>
         <li>TypeScript</li>
@@ -76,13 +78,13 @@ const About = () => {
 
 const ContentTemplate = (props: Contents) => {
   return (
-    <div id={ props.type } class="ccc fade-in grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 lg:gap-8 2xl:grid-cols-4 2xl:gap-10" style="display: none">
-      { props.contents.map((content) => (
+    <div id={props.type} class="ccc fade-in grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 lg:gap-8 2xl:grid-cols-4 2xl:gap-10" style="display: none">
+      {props.contents.map((content) => (
         <div class="content">
-          <img src={ content.thumbnail } class="h-auto w-auto" />
+          <img src={content.thumbnail} class="h-auto w-auto" />
           <div class="content-overview flex flex-col">
-            <p>{ content.title }</p>
-            <p class="text-gray-400">{ content.overview }</p>
+            <p>{content.title}</p>
+            <p class="text-gray-400">{content.overview}</p>
           </div>
         </div>
       ))}
@@ -90,20 +92,25 @@ const ContentTemplate = (props: Contents) => {
   )
 }
 
-
 const Footer = () => {
-  const year = (new Date()).getFullYear()
+  const year = new Date().getFullYear()
   return (
     <div class="footer flex justify-between items-center w-9/12 h-16 lg:flex-col lg:left-16 sm:w-11/12 lg:w-24 lg:top-[550px] lg:fixed lg:z-10">
       <div class="sns flex flex-row text-2xl">
-        <a href="https://twitter.com/ryoo141" target="_blanck" rel="noopener noreferrer"><i class="ti ti-brand-twitter hover:text-rblue" /></a>
-        <a href="https://github.com/ryoo14" target="_blanck" rel="noopener noreferrer"><i class="ti ti-brand-github hover:text-rblue" /></a>
-        <a href="https://www.instagram.com/ryoo141" target="_blanck" rel="noopener noreferrer"><i class="ti ti-brand-instagram hover:text-rblue" /></a>
-        <a href="https://atcoder.jp/users/ryoo14" target="_blanck" rel="noopener noreferrer"><i class="ti ti-code hover:text-rblue" /></a>
+        <a href="https://twitter.com/ryoo141" target="_blanck" rel="noopener noreferrer">
+          <i class="ti ti-brand-twitter hover:text-rblue" />
+        </a>
+        <a href="https://github.com/ryoo14" target="_blanck" rel="noopener noreferrer">
+          <i class="ti ti-brand-github hover:text-rblue" />
+        </a>
+        <a href="https://www.instagram.com/ryoo141" target="_blanck" rel="noopener noreferrer">
+          <i class="ti ti-brand-instagram hover:text-rblue" />
+        </a>
+        <a href="https://atcoder.jp/users/ryoo14" target="_blanck" rel="noopener noreferrer">
+          <i class="ti ti-code hover:text-rblue" />
+        </a>
       </div>
-      <div class="copyright flex flex-row">
-        © { year } ryoo 
-      </div>
+      <div class="copyright flex flex-row">© {year} ryoo</div>
     </div>
   )
 }
@@ -113,9 +120,9 @@ const app = new Hono()
 app.get("/", (c) => {
   return c.html(
     <Home>
-      <ContentTemplate { ...work } />
-      <ContentTemplate { ...use } />
-    </Home>
+      <ContentTemplate {...work} />
+      <ContentTemplate {...use} />
+    </Home>,
   )
 })
 
