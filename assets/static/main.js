b9d0dced8572bb1436c6b88814e21db408cf6eda
ryoo <anana12185@gmail.com>
2024-06-05 14:23:32 +0900

    refactor: separete links (#11)

--- assets/static/main.js
+++ assets/static/main.js
@@ -17,27 +17,33 @@ function setColor() {
 setColor();
 window.matchMedia('(prefers-color-scheme: dark)').addListener(setColor);
 
-window.addEventListener('DOMContentLoaded', () => { 
-  const about = document.getElementById("about");
-  about.classList.add("visible");
-});
+document.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    document.querySelector(".fade-in").classList.add("visible")
+  }, 10);
+})
 
-document.querySelectorAll('.sidebar-link').forEach(link => {
-  link.addEventListener('click', function (e) {
-    e.preventDefault();
-
-    document.querySelectorAll('.ccc').forEach(c => {
-      c.style.display = "none";
-      c.classList.remove("visible");
-    });
-
-    const targetId = this.getAttribute("data-target");
-    const targetContent = document.getElementById(targetId);
-    targetContent.style.display = "grid";
-
-    setTimeout(() => {
-      targetContent.classList.add("visible");
-    }, 10);
-  });
-});
+//window.addEventListener('DOMContentLoaded', () => { 
+//  const about = document.getElementById("about");
+//  about.classList.add("visible");
+//});
+//
+//document.querySelectorAll('.sidebar-link').forEach(link => {
+//  link.addEventListener('click', function (e) {
+//    e.preventDefault();
+//
+//    document.querySelectorAll('.ccc').forEach(c => {
+//      c.style.display = "none";
+//      c.classList.remove("visible");
+//    });
+//
+//    const targetId = this.getAttribute("data-target");
+//    const targetContent = document.getElementById(targetId);
+//    targetContent.style.display = "grid";
+//
+//    setTimeout(() => {
+//      targetContent.classList.add("visible");
+//    }, 10);
+//  });
+//});
 
--- src/main.tsx
+++ src/main.tsx
@@ -20,7 +20,6 @@ const Home = (props) => {
         <div class="container flex flex-col justify-start items-center w-full lg:flex-row lg:block mt-0 lg:mt-20">
           <SideBar />
           <div class="main-content w-9/12 sm:w-11/12 lg:w-9/12 lg:absolute mt-24 sm:mt-32 mb-10 lg:left-56 lg:mt-0">
-            <About />
             {props.children}
           </div>
           <Footer />
@@ -36,13 +35,13 @@ const SideBar = () => {
     <div class="sidebar flex flex-row justify-between items-center backdrop-blur-sm fixed top-0 lg:top-20 z-10 w-10/12 h-16 sm:w-11/12 sm:h-20 lg:h-28 lg:flex-col lg:left-16 lg:w-24">
       <object data="/static/portfoliop.svg" class="h-12 lg:mb-20" />
       <div class="links flex flex-row w-4/6 sm:w-3/6 lg:w-24 justify-between items-center lg:flex-col">
-        <a href="#" class="sidebar-link hover:text-rorange lg:mb-10" data-target="about">
+        <a href="/" class="sidebar-link hover:text-rorange lg:mb-10" data-target="about">
           About
         </a>
-        <a href="#" class="sidebar-link hover:text-rorange lg:mb-10" data-target="work">
+        <a href="/works" class="sidebar-link hover:text-rorange lg:mb-10" data-target="work">
           Works
         </a>
-        <a href="#" class="sidebar-link hover:text-rorange lg:mb-10" data-target="use">
+        <a href="/uses" class="sidebar-link hover:text-rorange lg:mb-10" data-target="use">
           Uses
         </a>
         <a href="https://blog.ryoo.cc" target="_blank" rel="noopener noreferrer" class="sidebar-exlink hover:text-rorange lg:mb-10">
@@ -55,7 +54,7 @@ const SideBar = () => {
 
 const About = () => {
   return (
-    <div id="about" class="ccc fade-in grid-cols-1 w-full break-words" style="display: grid;">
+    <div id="about" class="fade-in grid-cols-1 w-full break-words" style="display: grid;">
       <div class="flex h-12 items-center">
         <h2 class="text-2xl mr-2">Hi! I'm ryoo!</h2>
         <object data="/static/ryoop.svg" class="h-12" />
@@ -80,7 +79,7 @@ const About = () => {
 
 const ContentTemplate = (props: Contents) => {
   return (
-    <div id={props.type} class="ccc fade-in grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 lg:gap-8 2xl:grid-cols-4 2xl:gap-10" style="display: none">
+    <div id={props.type} class="fade-in grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 lg:gap-8 2xl:grid-cols-4 2xl:gap-10" style="display: grid">
       {props.contents.map((content) => (
         <div class="content">
           <a href={content.url} target="_blanck" rel="noopener noreferrer">
@@ -124,7 +123,30 @@ const app = new Hono()
 app.get("/", (c) => {
   return c.html(
     <Home>
+      <About />
+    </Home>,
+  )
+})
+
+app.get("/about", (c) => {
+  return c.html(
+    <Home>
+      <About />
+    </Home>,
+  )
+})
+
+app.get("/works", (c) => {
+  return c.html(
+    <Home>
       <ContentTemplate {...work} />
+    </Home>,
+  )
+})
+
+app.get("/uses", (c) => {
+  return c.html(
+    <Home>
       <ContentTemplate {...use} />
     </Home>,
   )
